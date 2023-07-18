4867ea72e72fe92ee8e95b67b0ba2cc0ddc95aaa
jiyong.yoon <jyyoon0615@naver.com>
2023-07-18 22:40:42 +0900

    django - Model upgrate 1

--- third/models.py
+++ third/models.py
@@ -7,6 +7,9 @@ class Restaurant(models.Model):
     name = models.CharField(max_length=30)
     address = models.CharField(max_length=200)
 
+    password = models.CharField(max_length=20, default=None, null=True) # 이 필드가 새로 추가되는 것이기 때문에, default 값을 설정해주지 않으면 기존에 입력되어있던 Data 값이 없기 때문에
+    image = models.CharField(max_length=500, default=None, null=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
