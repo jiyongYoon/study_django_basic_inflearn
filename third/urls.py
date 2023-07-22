9c814683c497908461e935d5e110f4b4c90d9b33
jiyong.yoon <jyyoon0615@naver.com>
2023-07-22 23:05:45 +0900

    django - view 나누기 ver.2
    - view 파일 기반

--- third/urls.py
+++ third/urls.py
@@ -1,16 +1,35 @@
 from django.urls import path
 
-from . import views
+# from . import views
+#
+# urlpatterns = [
+#     path('list/', views.list, name='list'),
+#     path('create/', views.create, name='restaurant-create'),
+#     path('update/', views.update, name='restaurant-update'),
+#     # path('detail/', views.detail, name='restaurant-detail'),
+#     path('delete/', views.delete, name='restaurant-delete'),
+#
+#     path('restaurant/<int:id>/', views.detail, name='restaurant-detail'),
+#     path('restaurant/<int:restaurant_id>/review/create/', views.review_create, name='review-create'),
+#     path('restaurant/<int:restaurant_id>/review/delete/<int:review_id>', views.review_delete, name='review-delete'),
+#     path('review/list/', views.review_list, name='review-list'),
+# ]
+
+
+from .views import base_views, restaurant_views, review_views
 
 urlpatterns = [
-    path('list/', views.list, name='list'),
-    path('create/', views.create, name='restaurant-create'),
-    path('update/', views.update, name='restaurant-update'),
-    # path('detail/', views.detail, name='restaurant-detail'),
-    path('delete/', views.delete, name='restaurant-delete'),
+    # base_views.py
+    path('list/', base_views.list, name='list'),
 
-    path('restaurant/<int:id>/', views.detail, name='restaurant-detail'),
-    path('restaurant/<int:restaurant_id>/review/create/', views.review_create, name='review-create'),
-    path('restaurant/<int:restaurant_id>/review/delete/<int:review_id>', views.review_delete, name='review-delete'),
-    path('review/list/', views.review_list, name='review-list'),
+    # restaurant_views.py
+    path('create/', restaurant_views.create, name='restaurant-create'),
+    path('update/', restaurant_views.update, name='restaurant-update'),
+    path('delete/', restaurant_views.delete, name='restaurant-delete'),
+    path('restaurant/<int:id>/', restaurant_views.detail, name='restaurant-detail'),
+
+    # review_views.py
+    path('restaurant/<int:restaurant_id>/review/create/', review_views.review_create, name='review-create'),
+    path('restaurant/<int:restaurant_id>/review/delete/<int:review_id>', review_views.review_delete, name='review-delete'),
+    path('review/list/', review_views.review_list, name='review-list'),
 ]
--- third/views/__init__.py
+++
@@ -1,3 +0,0 @@
-from .base_views import *
-from .restaurant_views import *
-from .review_views import *
